9539a7b1a3779355b4b7effd4e2d733f2002cd91
Michael Hum <michaelhum@cmail.carleton.ca>
2015-08-05 20:29:30 -0400

    Initial commit

---
+++ src/main/java/ca/carleton/bank/Launcher.java
@@ -0,0 +1,12 @@
+package ca.carleton.bank;
+
+/**
+ * Hello world!
+ */
+public class Launcher {
+
+    public static void main(final String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
